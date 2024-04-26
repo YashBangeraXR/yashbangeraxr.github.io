a3b3e86f5bc23f1919ceba3ec45e2dc449f535d2
YashBangera <yashbangera@crosscomm.com>
2024-04-26 15:56:20 -0600

    Added portal shader and interaction

--- components/app/menu.js
+++ components/app/menu.js
@@ -2,22 +2,5 @@
 AFRAME.registerComponent('menu', {
     init: function () {
         var el = this.el;
-        var menuBackGroundEl = document.createElement('a-entity');
-        menuBackGroundEl.setAttribute('geometry', {
-            primitive: 'box',
-            width: 1.5,
-            height: 1,
-            depth: 0.01
-        });
-        menuBackGroundEl.setAttribute('material', {
-            color: 'FFFFFF',
-            roughness: .1,
-            metalness: .6,
-            transparent: true,
-            alphaTest: 0.5,
-            opacity: 0.6
-        });
-        menuBackGroundEl.setAttribute('position', '0 0 -0.1');
-        el.appendChild(menuBackGroundEl);
     }
 });
--- components/app/template-button.js
+++ components/app/template-button.js
@@ -2,17 +2,30 @@
 AFRAME.registerComponent("template-button", {
     schema: {
       label: { default: "label" },
-      icon: { default: "#FSNG_Icon" },
+      icon: { default: "#FSNG_Icon-360" },
       targetTemplate: { default: "templates/FSNG.html" },
       scale: { default: "0.25 0.25 0.25" },
       toggable: { default: false },
+      type : {default: "circle"},
     },
     init: function () {
       console.log("icon: ", this.data.icon);
+      console.log("type: ", this.data.type);
       //Set mesh properties
       const el = this.el;
-      el.setAttribute("gltf-model", this.data.icon);
-      el.setAttribute("pressable", "");
+
+      if(this.data.type === "circle")
+      {
+        el.setAttribute("geometry", "primitive: circle; width: 1; height: 1d; depth: 1");
+        el.setAttribute("material", "shader: portal; color: white; pano: " + this.data.icon);
+      }
+      else if(this.data.type === "sphere")
+      {
+        el.setAttribute("geometry", "primitive: sphere; width: 1; height: 1; depth: 1");
+        el.setAttribute("material", "shader: flat; color: white; src: " + this.data.icon);
+      }
+
+      el.setAttribute("pressable");
       el.setAttribute("scale", this.data.scale);
   
       //set label properties
@@ -24,21 +37,33 @@ AFRAME.registerComponent("template-button", {
         align: "center",
       });
       labelEl.setAttribute("scale", "5 5 5");
-      labelEl.setAttribute("position", "0, -.55, 0");
+      labelEl.setAttribute("position", "0, -1.5, 0");
+
+
+      let initialScaleX = this.data.scale.split(" ")[0];
+      let initialScaleY = this.data.scale.split(" ")[1];
+      let initialScaleZ = this.data.scale.split(" ")[2];
+
+      let scaleMultiplier = 1.3;
+      let enlargedScaleX = initialScaleX * scaleMultiplier;
+      let enlargedScaleY = initialScaleY * scaleMultiplier;
+      let enlargedScaleZ = initialScaleZ * scaleMultiplier;
+      
+      let enlargedScale = enlargedScaleX + " " + enlargedScaleY + " " + enlargedScaleZ;
   
       //Add animations
       el.setAttribute(
         "animation__mouseenter",
-        "property: scale; to: .3 .3 .3; dur: 300; startEvents: mouseenter"
+        "property: scale; to: " + enlargedScale + "; dur: 300; startEvents: mouseenter"
       );
       el.setAttribute(
         "animation__mouseenter",
-        "property: scale; to: .3 .3 .3; dur: 300; startEvents: pressedstarted"
+        "property: scale; to: " + enlargedScale + "; dur: 300; startEvents: pressedstarted"
       );
   
       el.setAttribute(
         "animation__mouseleave",
-        "property: scale; to: .25 .25 .25; dur: 300; startEvents: mouseleave"
+        "property: scale; to: " + this.data.scale + "; dur: 300; startEvents: mouseleave"
       );
   
       el.setAttribute(
